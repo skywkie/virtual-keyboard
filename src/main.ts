2402c6ac91f2e8ace56c3bf4c7589d4ec778dd96
skywkie <dlunusin@gmail.com>
2025-07-21 02:40:26 +0300

    add locked focus of textarea

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import Keyboard from "./keyboard";
 
 const keyboard = new Keyboard("keyboard_container", "input_field");
 
+const textarea = document.getElementById("input_field") as HTMLTextAreaElement;
+
 window.addEventListener("DOMContentLoaded", () => {
   keyboard.init();
 });
+
+textarea.addEventListener("blur", () => {
+  textarea.focus();
+});
+
+// TODO: синхронизировать с реальной клавиатурой
+// TODO: добавить стрелочки и чтоб буква добавлялась там, где курсор

89707bb2bf05e9b319c0b35de2a4f8e15e3364c8
skywkie <dlunusin@gmail.com>
2025-07-21 18:07:32 +0300

    delete extra file with types

--- src/types.ts
+++
@@ -1,7 +0,0 @@
-interface HTMLCollectionOf<T extends Element> extends HTMLCollection {
-  item(index: number): T;
-  namedItem(name: string): T;
-  [index: number]: T;
-}
-
-export type { HTMLCollectionOf };

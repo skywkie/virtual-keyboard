ac33789ffb41224d1f4d1af4369b5393fa2a58e2
skywkie <dlunusin@gmail.com>
2025-07-22 23:16:13 +0300

    add mpre styles for clipboard button and add js logic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import "./style.css";
 import Keyboard from "./keyboard";
+import { useMessageByEvent } from "./ui";
+
+const wrapper = document.querySelector(".wrapper") as HTMLDivElement;
 
 const keyboard = new Keyboard("keyboard_container", "input_field");
+
+const clipboardButton = document.querySelector(".clipboard_button") as HTMLButtonElement;
 
 const textarea = document.getElementById("input_field") as HTMLTextAreaElement;
 
@@ -9,12 +14,24 @@ window.addEventListener("DOMContentLoaded", () => {
   keyboard.init();
 });
 
-textarea.addEventListener("blur", () => {
+textarea.onblur = () => {
   textarea.focus();
-});
+};
+
+clipboardButton.onclick = () => {
+  navigator.clipboard.writeText(textarea.value);
+
+  const previousMessage = document.querySelector(".message_dialog");
+  previousMessage && previousMessage.remove();
+
+  const message = useMessageByEvent("Text has copied to clipboard", 6000);
+
+  wrapper.appendChild(message);
+
+  message.show();
+};
 
 // TODO: сделать так, чтобы tabindex работал вместе с фокусом на textarea (важно, чтобы был курсор)
 // TODO: синхронизировать визуал с реальной клавиатурой
 // TODO: добавить стрелочки и чтоб буква добавлялась там, где курсор
-// TODO: создать кнопку для копирования текста
 // TODO: добавить смену темы: светлая/темная
---
+++ src/ui.ts
@@ -0,0 +1,20 @@
+const useMessageByEvent = (text: string, timeout: number) => {
+  const message = document.createElement("dialog");
+  const messageText = document.createElement("p");
+
+  message.className = "message_dialog";
+
+  message.style.animationDuration = `${timeout / 1000}s`;
+
+  messageText.textContent = text;
+
+  message.appendChild(messageText);
+
+  setTimeout(() => {
+    message.remove();
+  }, timeout);
+
+  return message;
+};
+
+export { useMessageByEvent };

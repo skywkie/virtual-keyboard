a481347d399bb875ccc40bfe55e00d23875d5f80
skywkie <dlunusin@gmail.com>
2025-07-31 21:09:12 +0300

    add 2 themes css

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,11 +1,13 @@
 const useToggleTheme = (defaultTheme?: "dark" | "light") => {
   const isDark = defaultTheme ? defaultTheme : window.matchMedia("(prefers-color-scheme: dark)").matches;
-  let userTheme: "dark" | "light" = isDark ? "dark" : "light";
+  let userTheme: "dark_mode" | "light_mode" = isDark ? "dark_mode" : "light_mode";
+
+  document.body.className = userTheme;
 
   const toggleTheme = () => {
-    userTheme === "dark" ? (userTheme = "light") : (userTheme = "dark");
+    userTheme === "dark_mode" ? (userTheme = "light_mode") : (userTheme = "dark_mode");
 
-    userTheme === "dark"
+    userTheme === "dark_mode"
       ? (document.body.className = "dark_mode")
       : (document.body.className = "light_mode");
   };
